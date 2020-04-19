89439da89d2e646be0d260376612f4c64536ed41
p1x3lc0w <p1x3lc0w@protonmail.com>
2020-04-19 17:13:12 +0200

    Add Meteor Break Particle

--- Assets/P1x3lc0w/LudumDare46/Code/Meteor.cs
+++ Assets/P1x3lc0w/LudumDare46/Code/Meteor.cs
@@ -14,11 +14,15 @@ namespace P1x3lc0w.LudumDare46
         public Transform blockContainer;
         public GameObject spriteGO;
         public AudioSource explosionAudioSource;
+        public ParticleSystem breakParticleSystem;
+        public TrailRenderer trailRenderer;
 #pragma warning restore CS0649
 
         public bool Broken { get; private set; }
 
         private Vector2 _velocity = Vector2.zero;
+
+        private float _particleTime;
 
         public void Start()
         {
@@ -34,6 +38,21 @@ namespace P1x3lc0w.LudumDare46
                 {
                     blockContainer.localPosition += (Vector3)(_velocity * Time.deltaTime);
                     _velocity.y += -0.1f * Time.deltaTime;
+                }
+                else
+                {
+                    _particleTime -= Time.deltaTime;
+                    
+                    if(_particleTime < -10.0f)
+                    {
+                        Destroy(gameObject);
+                    }
+                    if(breakParticleSystem != null && _particleTime < 0)
+                    {
+                        ParticleSystem.EmissionModule e = breakParticleSystem.emission;
+                        e.enabled = false;
+                        breakParticleSystem = null;
+                    }
                 }
             }
         }
@@ -57,8 +76,11 @@ namespace P1x3lc0w.LudumDare46
 
         void Break()
         {
+            breakParticleSystem.gameObject.SetActive(true);
+            _particleTime = 0.1f;
             explosionAudioSource.Play();
             spriteGO.SetActive(false);
+            trailRenderer.emitting = false;
             Broken = true;
         }
     }
